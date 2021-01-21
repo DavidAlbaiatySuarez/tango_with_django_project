3d742baf2044fbb4f279d35c7980b898230f591a
David <2474364A@student.gla.ac.uk>
2021-01-21 12:09:09 +0000

    chapter 3

---
+++ rango/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+'''
+Each view exists withing views.py as a series of individual functions
+We have now created one view called index.
+Each view takes at least one argument (an HttpRequest - convention to call
+it request) and return one HttpResponse (what we want to send to the client
+requesting the view.
+'''
+def index(request):
+    return HttpResponse("Rango says hey there partner!")
