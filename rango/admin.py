0eb011050e60f1805fbf22f78d138c9846344f7c
David <2474364A@student.gla.ac.uk>
2021-01-22 12:37:33 +0000

    chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from rango.models import Category, Page
+
+# we add these classes to the Django administration
+admin.site.register(Category)
+admin.site.register(Page)
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+# Class inherits from base class django.db.models.Model
+class Category(models.Model):
+    # since unique=True, every category name must be unique
+    # we can use default='value' so that field has a default value
+    # or whether a value for a field is allowed to be NULL (null=True)
+    name = models.CharField(max_length=128, unique=True)
+
+    # changes the category name from 'Categorys' to 'Categories'
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    # the foreign key allows a one-to-many relation with Category class
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128) # stores character data (string)
+    url = models.URLField() # stores resource URLs
+    views = models.IntegerField(default=0) # stores integers
+
+    def __str__(self):
+        return self.title
